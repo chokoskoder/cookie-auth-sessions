836c83288171a2cafaa72627d98f9b0c070e6dfe
chokoskoder <chokoskoder@gmail.com>
2025-01-18 00:21:05 +0530

    forgot some folders , added them now

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const application = require('../cookies/src/app.js')
 app = express();
 
 const port = process.env.PORT;
 
-try{
 
+app.use('/' , application);
+
+try{
+    app.listen(PORT);
 }
 catch(err){
-    console.log()
+    console.log("we are not able to connect to the port" , err)
 }
 
